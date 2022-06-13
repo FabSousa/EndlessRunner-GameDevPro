ac56c398f3ecd32a8e09eeeb9bdf5de2e8644b98
Fabio <f.sousasp@hotmail.com>
2022-06-12 22:19:54 -0300

    try again

---
+++ Assets/Runtime/Scripts/Level/MovingObstacle.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObstacle : Obstacle
+{
+    [SerializeField] private float movSpeed = 1;
+    private float positionT = 0;
+    private float laneDistanceX = 2;
+
+    void Update()
+    {
+        positionT += Time.deltaTime * movSpeed;
+        float lanePositionX = (Mathf.PingPong(positionT, 1) - 0.5f) * laneDistanceX * 2;
+
+        Vector3 pos = transform.position;
+        pos.x = lanePositionX;
+        transform.position = pos;
+    }
+}
